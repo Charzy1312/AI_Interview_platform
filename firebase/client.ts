d2406c7c25d60e8911bee1e55b6b1aa7d4b88e6d
Charlcleares <ibrahchars@gmail.com>
2025-03-24 01:00:36 +0530

    move the secret keys to env file

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_oXuXY0-8O8B290rsRSqb9EyG7ESFRwE",
-  authDomain: "codebuddy-e755b.firebaseapp.com",
-  projectId: "codebuddy-e755b",
-  storageBucket: "codebuddy-e755b.firebasestorage.app",
-  messagingSenderId: "483800260421",
-  appId: "1:483800260421:web:fe5c0e60bd05ffa862a82c",
-  measurementId: "G-B2SVMKK2BS"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, 
 };
 
 // Initialize Firebase
